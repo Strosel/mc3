5b002b19e2c3c90dd1ac3c05037917e321a9ad01
axel paulander <axel.paulander@gmail.com>
2025-01-02 11:21:17 +0100

    Example for issue

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flux issue";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flux.url = "github:IogaMaster/flux";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flux,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [flux.overlays.default];
+    };
+  in {
+    packages.${system}.default = pkgs.mkMinecraftServer {
+      name = "myminecraftserver";
+      src = ./.;
+      hash = "";
+    };
+  };
+}
